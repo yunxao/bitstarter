69c57a9650c3845299b0f6722d96e74a492e7e2f
Francisco Huertas <pacohuertas@gmail.com>
2013-07-04 16:25:19 +0000

    slsdsdklsad hiovwaeynaoynovyn8o

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  var cad = fs.readFile('index.html','utf8');
+  var cad = fs.readFile('./index.html','utf8');
   response.send("+"+cad);
 });
 
@@ -12,3 +12,4 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
